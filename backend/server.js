b30460fc0cf4413d8cb40c951d488516070d03cc
josemelendezCR <jose.melendez.respaldo@gmail.com>
2022-05-02 14:32:59 -0600

    Port config

--- backend/server.js
+++ backend/server.js
@@ -1,1 +1,9 @@
-console.log("Server running");
+const express = require('express');
+const dotenv = require('dotenv').config();
+
+const port = process.env.PORT || 5000;
+
+const app = express();
+
+// Set server port
+app.listen(port, () => console.log(`Server started on port ${port}`));
